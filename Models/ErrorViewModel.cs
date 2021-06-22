e0c2b674657922fbd1ccef0e730a43b14e2ab37f
Thiago Malaquias <tammalaquias@gmail.com>
2021-06-21 21:33:54 -0300

    init project api-cms

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace api_cms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
